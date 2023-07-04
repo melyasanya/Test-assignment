35ed9442d0c72e6954024d0dc33f40675281807e
melyasanya <werbolom@gmail.com>
2023-07-04 16:11:40 +0300

    404 correct

--- test-assignment/src/main.jsx
+++ test-assignment/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { persistor, store } from "./redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import App from "./App.jsx";
 import "./index.css";
@@ -12,9 +12,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <PersistGate persistor={persistor}>
       <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </React.StrictMode>
     </PersistGate>
   </Provider>
